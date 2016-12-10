a4a94f445768ab2676495d12dc5ff83a14494c1f
metinakkin <metin_akkin@hotmail.com>
2016-12-10 16:21:18 +0200

    Add files via upload

---
+++ A/A.ino
@@ -0,0 +1,28 @@
+#include<Servo.h>
+
+Servo sg90;
+int pos=0;
+const int escpin = 0;
+int val;   
+
+void setup()
+{
+sg90.attach(3);
+// pinMode(potpin, INPUT); 
+ sg90.write(0);
+    delay(300);
+    sg90.write(90);
+    delay(2250);
+
+}
+
+void loop()
+{
+  
+  
+    delay(2108);
+    sg90.write(0);
+    delay(300);
+    sg90.write(90);
+    delay(2108);
+}
